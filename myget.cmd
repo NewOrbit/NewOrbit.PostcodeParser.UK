d9b9424e7286e9f1c447359e9a26087a99094752
Frans Lytzen <flytzen@neworbit.co.uk>
2017-07-04 17:38:23 +0100

    Fixed typo

--- myget.cmd
+++ myget.cmd
@@ -3,4 +3,4 @@ dotnet restore
 dotnet test ./test/Tests/Tests.csproj
 if not "%errorlevel%"=="0" exit -1
 dotnet pack ./src/PostcodeParser.UK/PostcodeParser.UK.csproj -c Release --include-source
-nuget push \src\PostcodeParser.UK\bin\Release\* -Source 
+nuget push \src\PostcodeParser.UK\bin\Release\*
